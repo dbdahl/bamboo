199677d8bf6893ced1ef8143cff94435a7a8666d
David B. Dahl <david@ddahl.org>
2017-05-24 18:02:04 -0600

    Version 0.9.18

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   command.line.options <- c("-J-Xmx1G","-J-XX:ParallelGCThreads=1","-J-XX:ConcGCThreads=1")
-  # Note that shQuote below is needed for rscala <= 2.1.1 on Windows.  It can be removed when using later versions of rscala.
-  if ( packageVersion("rscala") <= '2.1.1' ) command.line.options <- shQuote(command.line.options)
   .rscalaPackage(pkgname,command.line.options=command.line.options)
+}
+
+.onUnload <- function(libpath) {
+  .rscalaPackageUnload()
 }
 
